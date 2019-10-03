bac947377c5640d0e504135810626444e056aee6
hyeokjoo <hyeokjoo@angelswing.io>
2019-10-03 13:33:23 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-631d8d36056aa663f4e2.js"
+    "url": "webpack-runtime-2cb9fa7c54042c814132.js"
   },
   {
     "url": "styles.16aaefe531b6b7795df4.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a405b0bbcd565e9c740585280d7475f8"
+    "revision": "5e67218cc2625d03ebdde024ebb229ad"
   },
   {
     "url": "component---src-pages-404-js-95f5274f97c9156b42f7.js"
