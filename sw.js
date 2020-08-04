4acec0b0378c4f527536f9d0ba9754adbb8c4b83
9oelM <hj923@hotmail.com>
2020-08-05 01:01:20 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-089b40b06c5ef79438cb.js"
+    "url": "webpack-runtime-b86c342bbab32fd341fd.js"
   },
   {
     "url": "styles.16aaefe531b6b7795df4.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2b993d779cb2643624edcdb540e9bcd3"
+    "revision": "561954847a010ebb4483cd853ec353b0"
   },
   {
     "url": "component---src-pages-404-js-95f5274f97c9156b42f7.js"
